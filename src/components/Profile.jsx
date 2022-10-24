e8814461ac2852d82fbbc2204ca83486b1e9cbd4
Virgold <virgoanchuks@gmail.com>
2022-10-24 16:00:45 +0100

    update profile image

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Img from '../assets/image1.jpeg'
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div className='d-flex flex-column align-items-center gap-3 py-4'>
+      <div className='profileImg'>
+        <img src={Img} alt="" id='profile__img' />
+      </div>
+      <div className='d-flex flex-row'>
+        <div id='twitter' size="">
+          <p>@KngVirgo</p>
+        </div>
+        <div className="ms-4 d-none" id='slack' size="">
+          <p>@SlackName</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
